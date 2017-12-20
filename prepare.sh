0c125746ab2e6e240701aee6bec91858c66ab786
fsword <li.jianye@gmail.com>
2017-12-20 19:06:17 +0800

    prepare.sh 支持指定 data folder

--- prepare.sh
+++ prepare.sh
@@ -1,12 +1,19 @@
 #!/bin/sh
 
+if [ "$1" = "" ]
+then
+  data_folder="/root/.ethereum"
+else
+  data_folder="$1"
+fi
+
 if [ -f 'pwd.txt' ]
 then
   echo
 else
   echo 'dummy user' > pwd.txt
-  /geth --password pwd.txt account new
+  /geth --datadir $data_folder --password pwd.txt account new
 fi
 
-grep -v '#' /static-nodes.json > /root/.ethereum/static-nodes.json
+grep -v '#' /static-nodes.json > $data_folder/static-nodes.json
 
